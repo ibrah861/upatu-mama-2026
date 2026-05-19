266f42b3439b72aa796e9bdd57f537143bee9975
ibrahim amour ali <ibra1980f@gmail.com>
2026-05-19 10:23:26 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import "./pages/css/userDashBoard.css";
 import { Signup } from "./pages/auth/Signup";
 import { NotFound } from "./pages/generalRoutes/NotFound";
 import { UserDashboard } from "./pages/protectedRoutes/UserDashboard";
+import { Payment } from "./pages/protectedRoutes/Payment";
+import { History } from "./pages/protectedRoutes/History";
+import { Invoice } from "./pages/protectedRoutes/Invoice";
 import { Navigation } from "./pages/generalRoutes/Navigation";
 
 import { Reducer } from "./pages/auth/Reducer";
@@ -33,6 +36,9 @@ function App() {
     <>
       <Routes>
         <Route path="/userdashboard" element={<Signup />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/invoice" element={<Invoice />} />
         <Route path="*" element={<NotFound />} />
         <Route path="reducer" element={<Reducer />} />
 
--- src/pages/generalRoutes/Nav_Part.jsx
+++ src/pages/generalRoutes/Nav_Part.jsx
@@ -7,25 +7,39 @@ import {
   FaFileInvoiceDollar,
 } from "react-icons/fa";
 
+import { Link } from "react-router-dom";
+
 export const Nav_Part = () => {
   return (
     <div className="nav-part">
-      <div className="icons">
-        <FaHome size={24} />
-        <p>Home</p>
-      </div>
-      <div className="icons">
-        <FaCreditCard size={24} />
-        <p>Payment</p>
-      </div>
-      <div className="icons">
-        <FaFileInvoiceDollar size={24} />
-        <p>Invoice</p>
-      </div>
-      <div className="icons">
-        <FaHistory size={24} />
-        <p>History</p>
-      </div>
+      <Link to={"/"}>
+        <div className="icons">
+          <FaHome size={24} />
+          <p>Home</p>
+        </div>
+      </Link>
+
+      <Link to={"/payment"}>
+        <div className="icons">
+          <FaCreditCard size={24} />
+          <p>Payment</p>
+        </div>
+      </Link>
+
+      <Link to={"/invoice"}>
+        <div className="icons">
+          <FaFileInvoiceDollar size={24} />
+          <p>Invoice</p>
+        </div>
+      </Link>
+
+      <Link to={"/history"}>
+        <div className="icons">
+          <FaHistory size={24} />
+          <p>History</p>
+        </div>
+      </Link>
+
       <div className="icons">
         <FaBars size={24} />
         <p>Menu</p>
